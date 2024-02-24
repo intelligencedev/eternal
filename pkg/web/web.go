035ea0f68526f116592d790ccf04185e2595affa
art.aquino <art.aquino@compute.tech>
2024-02-23 18:53:49 -0600

    Enhances web tool and adds basic comfyui workflow.

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -146,7 +146,7 @@ func ParseReaderView(r io.Reader) (string, error) {
 // isContentElement checks if the node is an element of interest for the reader view.
 func isContentElement(n *html.Node) bool {
 	switch n.DataAtom {
-	case atom.H1, atom.H2, atom.H3, atom.H4, atom.H5, atom.H6, atom.Article, atom.P, atom.Li:
+	case atom.H1, atom.H2, atom.H3, atom.H4, atom.H5, atom.H6, atom.Article, atom.P, atom.Li, atom.Code:
 		return true
 	case atom.A, atom.Strong, atom.Em, atom.B, atom.I:
 		return true
