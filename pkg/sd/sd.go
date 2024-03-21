5ffb7d59c07156c57fbdd367863b1121f381a393
art.aquino <art.aquino@compute.tech>
2024-03-20 20:57:13 -0500

    Fixes paths.

--- pkg/sd/sd.go
+++ pkg/sd/sd.go
@@ -72,8 +72,7 @@ func BuildCommand(dataPath string, params SDParams) *exec.Cmd {
 	//modelPath := filepath.Join(dataPath, "models/StableDiffusion/sd15/dreamshaper_8_q5_1.gguf")
 	modelPath := filepath.Join(dataPath, "models/dreamshaper-8-sd15/128713")
 	outPath := filepath.Join(dataPath, "web/img/sd_out.png")
-	//cmdPath := filepath.Join(dataPath, "sd/sd")
-	cmdPath := filepath.Join(dataPath, "sd")
+	cmdPath := filepath.Join(dataPath, "sd/sd")
 
 	pterm.Println("Command:", cmdPath)
 
