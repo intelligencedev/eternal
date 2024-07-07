6346f1bfca604958040b59befd01bf4564d31ed8
Art Aquino <art.aquino@compute.tech>
2024-07-06 23:52:14 -0500

    Adds comfyui workflow

--- handlers_tools.go
+++ handlers_tools.go
@@ -28,6 +28,327 @@ import (
 
 const serverAddress = "192.168.0.148:8188"
 
+const promptText = `{
+  "20": {
+    "inputs": {
+      "ckpt_name": "pixart/diffusion_pytorch_model.safetensors",
+      "model": "PixArtMS_Sigma_XL_2"
+    },
+    "class_type": "PixArtCheckpointLoader",
+    "_meta": {
+      "title": "PixArt Checkpoint Loader"
+    }
+  },
+  "65": {
+    "inputs": {
+      "samples": [
+        "345",
+        0
+      ],
+      "vae": [
+        "359",
+        0
+      ]
+    },
+    "class_type": "VAEDecode",
+    "_meta": {
+      "title": "VAE Decode"
+    }
+  },
+  "144": {
+    "inputs": {
+      "t5v11_name": "model-00001-of-00002.safetensors",
+      "t5v11_ver": "xxl",
+      "path_type": "folder",
+      "device": "gpu",
+      "dtype": "auto (comfy)"
+    },
+    "class_type": "T5v11Loader",
+    "_meta": {
+      "title": "T5v1.1 Loader"
+    }
+  },
+  "196": {
+    "inputs": {
+      "text": "a weather worn statue of medusa in the middle of a dark cavern with volumetric light shining upon it as etheral mist envelops the scene"
+    },
+    "class_type": "CR Text",
+    "_meta": {
+      "title": "Base Prompt"
+    }
+  },
+  "197": {
+    "inputs": {
+      "text": "((Signature, out of frame, hanging on wall, (white bars), deformed face, extra limbs, text, cartoon, poorly drawn hands, high contrast, poorly drawn eyes, bad eyes, ugly, poorly drawn face, poorly drawn hands, bad proportions, plastic, American flag, asian, large lips, anime, gloves, ((large head, oversized head)), chin dimple, (((long neck))), facial hair, wrinkly clothes, shiny skin, mustache, beard, old, neon, ((gloves, popped up collar, oversized collar)), black and white, glossy, shiny, bright, white lighting, cross-eyed, brown coat, tan coat, puffy coat))"
+    },
+    "class_type": "CR Text",
+    "_meta": {
+      "title": "Negative Prompt"
+    }
+  },
+  "224": {
+    "inputs": {
+      "IMAGE": [
+        "65",
+        0
+      ]
+    },
+    "class_type": "Anything Everywhere",
+    "_meta": {
+      "title": "Anything Everywhere"
+    }
+  },
+  "286": {
+    "inputs": {
+      "seed": 443241436811402
+    },
+    "class_type": "Seed Everywhere",
+    "_meta": {
+      "title": "Seed Everywhere"
+    }
+  },
+  "287": {
+    "inputs": {},
+    "class_type": "RandomNoise",
+    "_meta": {
+      "title": "RandomNoise"
+    }
+  },
+  "293": {
+    "inputs": {
+      "int": 50
+    },
+    "class_type": "Int Literal",
+    "_meta": {
+      "title": "Steps"
+    }
+  },
+  "345": {
+    "inputs": {
+      "add_noise": true,
+      "noise_is_latent": false,
+      "noise_type": "power",
+	  "noise_seed": "443241436811402",
+      "cfg": 7,
+      "model": [
+        "356",
+        0
+      ],
+      "positive": [
+        "367",
+        0
+      ],
+      "negative": [
+        "368",
+        0
+      ],
+      "sampler": [
+        "369",
+        0
+      ],
+      "sigmas": [
+        "351",
+        0
+      ],
+      "latent_image": [
+        "221:1",
+        0
+      ]
+    },
+    "class_type": "SamplerCustomNoise",
+    "_meta": {
+      "title": "SamplerCustomNoise"
+    }
+  },
+  "351": {
+    "inputs": {
+      "steps": [
+        "293",
+        0
+      ],
+      "sigma_max": [
+        "354",
+        0
+      ],
+      "sigma_min": [
+        "355",
+        0
+      ],
+      "rho": 7
+    },
+    "class_type": "KarrasScheduler",
+    "_meta": {
+      "title": "KarrasScheduler"
+    }
+  },
+  "352": {
+    "inputs": {
+      "custom_sigmas_manual_schedule": "sigmax",
+      "steps": 1,
+      "sgm": true,
+      "model": [
+        "20",
+        0
+      ]
+    },
+    "class_type": "Manual scheduler",
+    "_meta": {
+      "title": "Manual scheduler"
+    }
+  },
+  "353": {
+    "inputs": {
+      "custom_sigmas_manual_schedule": "sigmin",
+      "steps": 1,
+      "sgm": true,
+      "model": [
+        "20",
+        0
+      ]
+    },
+    "class_type": "Manual scheduler",
+    "_meta": {
+      "title": "Manual scheduler"
+    }
+  },
+  "354": {
+    "inputs": {
+      "model": [
+        "20",
+        0
+      ],
+      "sigmas": [
+        "352",
+        0
+      ]
+    },
+    "class_type": "Get sigmas as float",
+    "_meta": {
+      "title": "Get sigmas as float"
+    }
+  },
+  "355": {
+    "inputs": {
+      "model": [
+        "20",
+        0
+      ],
+      "sigmas": [
+        "353",
+        0
+      ]
+    },
+    "class_type": "Get sigmas as float",
+    "_meta": {
+      "title": "Get sigmas as float"
+    }
+  },
+  "356": {
+    "inputs": {
+      "hard_mode": true,
+      "boost": false,
+      "model": [
+        "20",
+        0
+      ]
+    },
+    "class_type": "Automatic CFG",
+    "_meta": {
+      "title": "Automatic CFG"
+    }
+  },
+  "359": {
+    "inputs": {
+      "vae_name": "pixart/diffusion_pytorch_model.safetensors"
+    },
+    "class_type": "VAELoader",
+    "_meta": {
+      "title": "Load VAE"
+    }
+  },
+  "367": {
+    "inputs": {
+      "text": [
+        "196",
+        0
+      ],
+      "T5": [
+        "144",
+        0
+      ]
+    },
+    "class_type": "T5TextEncode",
+    "_meta": {
+      "title": "T5 Text Encode"
+    }
+  },
+  "368": {
+    "inputs": {
+      "text": [
+        "197",
+        0
+      ],
+      "T5": [
+        "144",
+        0
+      ]
+    },
+    "class_type": "T5TextEncode",
+    "_meta": {
+      "title": "T5 Text Encode"
+    }
+  },
+  "369": {
+    "inputs": {
+      "sampler_name": "euler"
+    },
+    "class_type": "KSamplerSelect",
+    "_meta": {
+      "title": "KSamplerSelect"
+    }
+  },
+  "374": {
+    "inputs": {
+      "filename_prefix": "eternal",
+      "images": [
+        "65",
+        0
+      ]
+    },
+    "class_type": "SaveImage",
+    "_meta": {
+      "title": "Save Image"
+    }
+  },
+  "221:0": {
+    "inputs": {
+      "model": "PixArtMS_Sigma_XL_2",
+      "ratio": "1.00"
+    },
+    "class_type": "PixArtResolutionSelect",
+    "_meta": {
+      "title": "PixArt Resolution Select"
+    }
+  },
+  "221:1": {
+    "inputs": {
+      "width": [
+        "221:0",
+        0
+      ],
+      "height": [
+        "221:0",
+        1
+      ],
+      "batch_size": 1
+    },
+    "class_type": "EmptyLatentImage",
+    "_meta": {
+      "title": "Empty Latent Image"
+    }
+  }
+}`
+
 type Image struct {
 	Filename  string `json:"filename"`
 	Subfolder string `json:"subfolder"`
@@ -548,113 +869,6 @@ func handleRoleSelection(config *AppConfig) fiber.Handler {
 }
 
 func performImageGen(c *fiber.Ctx, imgPath string, chatMessage string) string {
-	promptText := `{
-  "3": {
-    "inputs": {
-      "seed": 156680208700286,
-      "steps": 20,
-      "cfg": 8,
-      "sampler_name": "euler",
-      "scheduler": "normal",
-      "denoise": 1,
-      "model": [
-        "4",
-        0
-      ],
-      "positive": [
-        "6",
-        0
-      ],
-      "negative": [
-        "7",
-        0
-      ],
-      "latent_image": [
-        "5",
-        0
-      ]
-    },
-    "class_type": "KSampler",
-    "_meta": {
-      "title": "KSampler"
-    }
-  },
-  "4": {
-    "inputs": {
-      "ckpt_name": "canvasdarkxl_v10.safetensors"
-    },
-    "class_type": "CheckpointLoaderSimple",
-    "_meta": {
-      "title": "Load Checkpoint"
-    }
-  },
-  "5": {
-    "inputs": {
-      "width": 512,
-      "height": 512,
-      "batch_size": 1
-    },
-    "class_type": "EmptyLatentImage",
-    "_meta": {
-      "title": "Empty Latent Image"
-    }
-  },
-  "6": {
-    "inputs": {
-      "text": "beautiful scenery nature glass bottle landscape, , purple galaxy bottle,",
-      "clip": [
-        "4",
-        1
-      ]
-    },
-    "class_type": "CLIPTextEncode",
-    "_meta": {
-      "title": "CLIP Text Encode (Prompt)"
-    }
-  },
-  "7": {
-    "inputs": {
-      "text": "text, watermark",
-      "clip": [
-        "4",
-        1
-      ]
-    },
-    "class_type": "CLIPTextEncode",
-    "_meta": {
-      "title": "CLIP Text Encode (Prompt)"
-    }
-  },
-  "8": {
-    "inputs": {
-      "samples": [
-        "3",
-        0
-      ],
-      "vae": [
-        "4",
-        2
-      ]
-    },
-    "class_type": "VAEDecode",
-    "_meta": {
-      "title": "VAE Decode"
-    }
-  },
-  "9": {
-    "inputs": {
-      "filename_prefix": "eternal",
-      "images": [
-        "8",
-        0
-      ]
-    },
-    "class_type": "SaveImage",
-    "_meta": {
-      "title": "Save Image"
-    }
-  }
-}`
 
 	var prompt map[string]interface{}
 	err := json.Unmarshal([]byte(promptText), &prompt)
@@ -663,8 +877,8 @@ func performImageGen(c *fiber.Ctx, imgPath string, chatMessage string) string {
 		return ""
 	}
 
-	prompt["6"].(map[string]interface{})["inputs"].(map[string]interface{})["text"] = chatMessage
-	prompt["3"].(map[string]interface{})["inputs"].(map[string]interface{})["seed"] = 5
+	prompt["196"].(map[string]interface{})["inputs"].(map[string]interface{})["text"] = chatMessage
+	prompt["286"].(map[string]interface{})["inputs"].(map[string]interface{})["noise_seed"] = 5
 
 	res, err := queuePrompt(prompt, uuid.New().String())
 	if err != nil {
